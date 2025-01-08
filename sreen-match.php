2f89d2764c0eb40f934b2be4c20b05e79e7c101b
SeuNome <jfalasco88@gmail.com>
2025-01-08 13:28:42 -0300

    Genero de filmes

--- sreen-match.php
+++ sreen-match.php
@@ -5,7 +5,11 @@ echo "Bem vindo ao screen match! \n";
 //Variáveis
 
 $nomeFilme = "Top Gun - Maverick";
+$nomeFilme = "Thor: Ragnarok";
+$nomeFilme = "Se beber não case";
+
 $anoLancamento = $argv[1] ?? 2022; //Pega a variável do terminal se tiver e se não tiver puxa o 2022
+
 
 //Tipos de somadores
 
@@ -27,3 +31,20 @@ $incluidoNoPlano = $planoPrime || $anoLancamento < 2020 ;
 echo "Nome do filme: " . $nomeFilme . "\n";
 echo "Nota do filme: $notaFilme \n";
 echo "Ano de lançamento: $anoLancamento\n";
+
+if ($anoLancamento > 2022) {
+    echo "Esse filme é lançamento\n";
+} else if ($anoLancamento > 2020 && $anoLancamento <= 2022) {
+    echo "Esse filme ainda é novo\n";
+} else {
+    echo "Esse filme não é lançamento\n";
+}
+
+$genero = match ($nomeFilme) {
+    "Top Gun - Maverick" => "ação",
+    "Thor: Ragnarok" => "Heróis",
+    "Se beber não case" => "Comédia",
+    default => "Gênero desconhecido"
+};
+
+echo "O gênero do filme é: $genero";
