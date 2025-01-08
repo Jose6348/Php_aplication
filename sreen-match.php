86130e13c61f64000806bdff623665f91074cc74
SeuNome <jfalasco88@gmail.com>
2025-01-08 12:52:02 -0300

    Exercises

--- sreen-match.php
+++ sreen-match.php
@@ -1,10 +1,13 @@
 <?php
 
-echo "Bem vindo ao screen match!
-";
+echo "Bem vindo ao screen match! \n";
+
+//Variáveis
 
 $nomeFilme = "Top Gun - Maverick";
-$anoLancamento = 2022;
+$anoLancamento = $argv[1] ?? 2022; //Pega a variável do terminal se tiver e se não tiver puxa o 2022
+
+//Tipos de somadores
 
 $somaDeNotas = 9;
 $somaDeNotas += 9;
@@ -19,8 +22,8 @@ $planoPrime = true;
 
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020 ;
 
+//Concatenação de variáveis
 
-echo $notaFilme;
-
-
-
+echo "Nome do filme: " . $nomeFilme . "\n";
+echo "Nota do filme: $notaFilme \n";
+echo "Ano de lançamento: $anoLancamento\n";
